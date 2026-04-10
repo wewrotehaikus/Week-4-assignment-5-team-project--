baafaaf35b987cd3a9f7e46fdd23858bade47917
Andre Gonzalez <agonzalez141414@gmail.com>
2026-04-09 22:40:31 -0700

    first commit?

---
+++ SortSearch.h
@@ -0,0 +1,11 @@
+/* ---------------------------------------------------------------------------
+** I attest that this following code represents my own work and is subject to
+** the plagiarism policy found in the course syllabus.
+**
+**       Class: CSC 252 - Programming in C++
+**  Assignment: Assignment 5
+**        File: SortSearch.h
+** Description: 
+**      Author: Karon Eley, Andre Gonzalez, Calvin Hart, Robert Wilson
+**        Date: 09 April 26
+** -------------------------------------------------------------------------*/
